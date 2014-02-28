a89bf8ffc33eb029f2607dbfbf544f156a2927f6
iliastsangaris <iliastsangaris@gmail.com>
2014-02-28 12:13:00 -0500

    User unit tests, db and model settup

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe User do 
+
+  it { should have_many (:stories) }
+  it { should have_many (:stories).through(:votes) }
+  it { should have_many (:stories).through(:comments) }
+
+  it { should validate_presence_of (:email) }
+  it { should validate_presence_of (:user_name) }
+  it { should validate_presence_of (:password_digest) }
+  it { should validate_presence_of (:created_at) }
+  it { should validate_presence_of (:updated_at) }
+
+  it { should validate_uniqueness_of (:email) }
+  it { should validate_uniqueness_of (:user_name) }
+
+end
